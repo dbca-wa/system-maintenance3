305a097814288b729a5b9a276d23210187e83e9d
Jason Moore <jason.moore@dbca.wa.gov.au>
2022-11-04 20:07:35 +0800

    Clean up Settings

--- queuewaiting/settings.py
+++ queuewaiting/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xq=!j*y8k59&9f3z07zuabnpw$0d379w85h#-f-lg64ybi^9=5'
+SECRET_KEY = env('SECRET_KEY','thisisnotkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', False)
